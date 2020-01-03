3cbd159f74790416ec8c1b546f3ee85b5587a789
viplav.jain <viplav.jain@DEL1-LHP-N61492.synapse.com>
2020-01-03 10:27:24 +0530

    Adding @ResponseBody annotation which returns the response in the JSON format.

--- plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.plantplaces.dto.SpecimenDTO;
 import com.plantplaces.service.ISpecimenService;
@@ -15,28 +16,29 @@ public class PlantPlacesController
 {
 	@Autowired
 	private ISpecimenService specimenServiceStub;
-    
+
 	@RequestMapping(value="/start",method=RequestMethod.GET)
-	public String read(Model model)
-  {
-	SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
-	model.addAttribute("specimenDTO", specimenDTO);
-	return "start";  
-  }
-	
+	@ResponseBody
+	public SpecimenDTO read(Model model)
+	{
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return specimenDTO ;  
+	}
+
 	@RequestMapping(value="/addSpecimen",method=RequestMethod.GET)
-	public String addSpecimen(Model model, @RequestParam(value="latitude") String latitude)
-  {
-	SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
-	specimenDTO.setLatitute(latitude);
-	model.addAttribute("specimenDTO", specimenDTO);
-	return "start";  
-  }
-    
-    @RequestMapping("/")
+	public String addSpecimen(Model model, @RequestParam(value="latitude",required=false) String latitude)
+	{
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setLatitute(latitude);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return "start";  
+	}
+
+	@RequestMapping("/")
 	public String index()
-  {
-	return "start";  
-  }
-	
+	{
+		return "start";  
+	}
+
 }
